f8c0aff0062b62bda1ecbe27883badd081ec59c1
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2023-12-27 09:56:44 +0100

    Add Authentification controller

---
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/AuthController.java
@@ -0,0 +1,54 @@
+package com.openclassrooms.mddapi.controllers;
+
+import com.openclassrooms.mddapi.paylod.request.LoginRequest;
+import com.openclassrooms.mddapi.paylod.request.RegisterRequest;
+import com.openclassrooms.mddapi.paylod.response.JwtResponse;
+import com.openclassrooms.mddapi.services.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+
+/**
+ * Controller handling authentication operations such as user registration and login.
+ */
+
+@RestController
+@Slf4j
+@RequestMapping(value = "auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+
+    /**
+     * Registers a new user and returns an authentication token.
+     *
+     * @param registerRequest The details of the user to register.
+     * @return A ResponseEntity containing the authentication token for the newly registered user.
+     */
+    @PostMapping("/register")
+    public ResponseEntity<JwtResponse> createUser(@RequestBody @Valid RegisterRequest registerRequest) {
+        String token = userService.registerUser(registerRequest);
+        JwtResponse jwtResponse = new JwtResponse(token);
+        return ResponseEntity.ok(jwtResponse);
+    }
+
+    /**
+     * Authenticates a user and returns an authentication token.
+     *
+     * @param loginRequest The details of the user for logging in.
+     * @return A ResponseEntity containing the authentication token for the logged-in user.
+     */
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> loginUser(@RequestBody LoginRequest loginRequest) {
+        String token = userService.loginUser(loginRequest);
+        JwtResponse jwtResponse = new JwtResponse(token);
+        return ResponseEntity.ok(jwtResponse);
+    }
+}
+
