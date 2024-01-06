88b8810ec9b8c8355990edd63e473559e963dca4
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2024-01-06 10:05:54 +0100

    Docs: add comments

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -4,6 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
+/**
+ * Defines the routes for the application.
+ * The array of `Routes` outlines the route configurations for the application.
+ * Each `Route` maps a URL path to a component.
+ *
+ * @type {Routes}
+ */
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * @component HomeComponent
+ * @selector app-home
+ *
+ * @description
+ * The HomeComponent provides the main view for the user when they navigate to the root of the application.
+ * It serves as the landing page of the application and is associated with the 'app-home' selector.
+ */
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
