0c0e333646979989bab517243ec57598040d09fd
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2024-01-06 10:29:20 +0100

    Fix: bug path

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
 import { HeaderAuthComponent } from './header-auth/header-auth.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
   declarations: [
