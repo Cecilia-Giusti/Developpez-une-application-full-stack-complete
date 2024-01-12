c173c2364fdc6bdbc8257f62cd41e0106ffa5a2f
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2024-01-12 09:59:28 +0100

    Add dashboard component and routing

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 /**
  * Defines the routes for the application.
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
