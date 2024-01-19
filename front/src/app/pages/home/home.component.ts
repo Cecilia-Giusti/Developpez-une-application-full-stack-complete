be8bb7bdecd1af5af44afdfd2e214f4da71ab634
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2024-01-19 10:48:41 +0100

    Improve life cycle

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 /**
  * @component HomeComponent
@@ -13,8 +13,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   constructor() {}
-
-  ngOnInit(): void {}
 }
